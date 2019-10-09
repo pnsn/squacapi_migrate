32f228f1126c7a43c18ffa8f8bbdb4a255a33f87
Jon Connolly <jontconnolly@gmail.com>
2019-10-09 15:50:52 -0700

    develop migration for station_metrics:measurments to measurement:measurement

--- migrate_measurements.py
+++ migrate_measurements.py
@@ -1,9 +1,12 @@
 import os
 import psycopg2
 import psycopg2.extras
-from squacapipy.squacapi import Metric, Measurement, Channel, Network
+from squacapipy.squacapi import Metric, Channel, Network, Measurement
+from datetime import datetime
+import pytz
+import argparse
 
-# for psycopg2
+# for psycopg2  
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWD = os.environ.get('DB_PASSWD')
@@ -15,9 +18,11 @@ API_TOKEN = os.getenv('SQUAC_API_TOKEN')
 API_BASE_URL = os.getenv('SQUAC_API_BASE')
 
 '''
+Scipt to read data from prototype station_metrics db into squac using
+squacapipy
+
 To run:
-source .env && python migrate_measurements.py
-* assumes all env vars are in file called .env
+ See usage
 
 Desired metrics
 metric_name             metric_id    threshold (per hour)
@@ -103,15 +108,14 @@ def make_measurement_payload(row, lookup):
         except IndexError:
             print("Channel {} not found".format(channel_key))
             return None
-            
 
     channel_id = lookup[channel_key]
     metric_id = lookup[row[12]]
     value, starttime, endtime = row[3], row[4], row[5]
 
     payload = {
-        'channel_id': channel_id,
-        'metric_id': metric_id,
+        'channel': channel_id,
+        'metric': metric_id,
         'value': value,
         'starttime': starttime,
         'endtime': endtime
@@ -120,19 +124,44 @@ def make_measurement_payload(row, lookup):
     return payload
 
 
-# no spaces in in param
-networks = Network().get(network='uw,uo,cc')
-if networks.status_code != 200:
-    print('Error {}'.format(networks.body))
+parser = argparse.ArgumentParser(
+    description="""Migrate station_metrics db measurements into squac db""",
+    usage="""source .env && python migrate_measurements.py
+            --networks=CC,UW,UO
+            --metrics=snr20_0p34cmHP,snr20_0p34cmHP,pctavailable,ngaps
+            --endtime=[YYYY-mm-dd]""")
+
+parser.add_argument('--networks', required=True,
+                    help="Comma seperated list of networks")
+parser.add_argument('--metrics', required=True,
+                    help="Comma seperated list of metrics")
+parser.add_argument('--starttime', required=True,
+                    help="metrics created at and after, inclusive")
+parser.add_argument('--endtime', required=True,
+                    help="metrics created beforem exclusive")                    
+args = parser.parse_args()
+
+
+networks_tup = tuple(n.upper() for n in args.networks.split(","))
+metrics_tup = tuple(m for m in args.metrics.split(","))
+print(networks_tup)
+print(metrics_tup)
+
+year, month, day = args.starttime.split("-")
+starttime = datetime(int(year), int(month), int(day), tzinfo=pytz.UTC)
+year, month, day = args.endtime.split("-")
+endtime = datetime(int(year), int(month), int(day), tzinfo=pytz.UTC)
+nets_squac = Network().get(network=args.networks.lower())
+if nets_squac.status_code != 200:
+    print('Error {}'.format(nets_squac.body))
 
 metrics = Metric().get(
-    name='pctavailable,ngaps,snr20_0p34cmHP,RMSduration_0p07cm')
+    name=args.metrics)
 # create hash of unique keys for quick lookup
 lookup = {}
 for m in metrics.body:
     key = m['name']
     lookup[key] = m['id']
-print(lookup)
 cursor = None
 try:
     connection = psycopg2.connect(dbname=DB_NAME,
@@ -142,7 +171,7 @@ try:
 
     # use DictCursor to access by column name
     cursor = connection.cursor()
-    sql_query = """ SELECT
+    sql_query = '''SELECT
                         m.*,
                         s.net as net,
                         s.sta as sta,
@@ -152,21 +181,41 @@ try:
                     FROM measurements m
                     JOIN sncls s ON s.id = m.sncl_id
                     JOIN metrics  ON metrics.id = m.metric_id
-                    WHERE s.net IN ('UW', 'UO', 'CC')
-                    AND metrics.metric IN(
-                        'snr20_0p34cmHP', 'snr20_0p34cmHP',
-                        'pctavailable', 'ngaps'
-                    )
-                    AND m.starttime = '2019-10-03';
-                """
-    cursor.execute(sql_query)
+                    WHERE s.net IN %s
+                    AND metrics.metric IN %s
+                    AND m.starttime >= %s
+                    AND m.starttime < %s;
+                '''
+
+    cursor.execute(sql_query, (networks_tup, metrics_tup, starttime, endtime))
+    # print(cursor.query)
     measurements = cursor.fetchall()
     payloads = []
     for m in measurements:
         payload = make_measurement_payload(m, lookup)
         if payload:
             payloads.append(payload)
-    print(payloads)
+    if len(payload) > 0:
+        # slice payloads into 100's
+        slicey = 100
+        end = slicey
+        start = 0
+        while start < len(payloads):
+            collection = payloads[start:end]
+            m = Measurement().post(collection)
+            if m.status_code != 201:
+                print("Error {} on bulk post".format(m.status_code))
+                print(m.body[0])
+                print("Trying single posts...")
+                for p in collection:
+                    m = Measurement().post(p)
+                    if m.status_code != 201:
+                        print("Error {} on single post".format(m.status_code))
+                        print(m.body)
+            start += slicey
+            end += slicey
+
+
 except psycopg2.Error as error:
     print("Error connecting to station_mentrics DB. Error: {}".format(error))
 finally:
